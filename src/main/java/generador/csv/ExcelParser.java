5327551a94637c2fe999f0c3dadbd71e7086ef21
Matias Salomone <matisalomone@hotmail.com>
2023-08-17 12:27:34 -0300

    SomeRandomChange

--- src/main/java/generador/csv/ExcelParser.java
+++ src/main/java/generador/csv/ExcelParser.java
@@ -1,4 +1,4 @@
-package generador.csv;
+package generador.csv ;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -13,18 +13,13 @@ import org.apache.poi.ss.usermodel.*;
 
 public class ExcelParser {
     public static void main(String[] args) {
-
-
         try {
-
-
             Properties prop = new Properties();
             loadProperties(prop);
-            // get the property value and print it out
-            final String PATH_TO_WALK = prop.getProperty("path.to.walk") ;
-            final String PATH_TO_WRITE = prop.getProperty("path.to.write") ;
-            Boolean processed = false ;
-
+            // Get the property values
+            final String PATH_TO_WALK = prop.getProperty("path.to.walk");
+            final String PATH_TO_WRITE = prop.getProperty("path.to.write");
+            Boolean processed = false;
 
             List<File> filesInFolder = Files.walk(Paths.get(PATH_TO_WALK))
                     .filter(Files::isRegularFile)
@@ -35,19 +30,14 @@ public class ExcelParser {
             Sheet destinationSheet = destinationWorkbook.createSheet("ParsedData");
             int rowIndex = 0;
 
-            System.out.println("============= FILES IN PATH =============");
             for (File file : filesInFolder) {
-
-                System.out.println(file.toString());
-
                 if (file.isFile() && file.getName().endsWith(".xlsx")) {
-
                     Workbook sourceWorkbook = WorkbookFactory.create(file);
 
                     for (Sheet sourceSheet : sourceWorkbook) {
                         for (Row sourceRow : sourceSheet) {
-                            Cell telephoneCell = sourceRow.getCell(0);
-                            Cell personCell = sourceRow.getCell(1);
+                            Cell telephoneCell = sourceRow.getCell(2); // Adjust column index for telephone number
+                            Cell personCell = sourceRow.getCell(3); // Adjust column index for person name
 
                             if (telephoneCell != null && personCell != null) {
                                 String telephone = telephoneCell.getStringCellValue();
@@ -55,13 +45,26 @@ public class ExcelParser {
 
                                 String normalizedTelephone = normalizeTelephone(telephone);
 
-                                Row destinationRow = destinationSheet.createRow(rowIndex++);
-                                Cell destinationTelephoneCell = destinationRow.createCell(0);
-                                Cell destinationPersonCell = destinationRow.createCell(1);
+                                // Check if the person already exists in the destination sheet
+                                int existingRowIndex = findExistingPerson(destinationSheet, person);
 
-                                destinationTelephoneCell.setCellValue(normalizedTelephone);
-                                destinationPersonCell.setCellValue(person);
-                                processed = true ;
+                                if (existingRowIndex == -1) {
+                                    // Person does not exist, create a new row
+                                    Row destinationRow = destinationSheet.createRow(rowIndex++);
+                                    Cell destinationTelephoneCell = destinationRow.createCell(6); // Column G
+                                    Cell destinationPersonCell = destinationRow.createCell(2); // Column C
+
+                                    destinationTelephoneCell.setCellValue(normalizedTelephone);
+                                    destinationPersonCell.setCellValue(person);
+                                } else {
+                                    // Person exists, update the telephone number
+                                    Row existingRow = destinationSheet.getRow(existingRowIndex);
+                                    Cell existingTelephoneCell = existingRow.getCell(6); // Column G
+
+                                    existingTelephoneCell.setCellValue(normalizedTelephone);
+                                }
+
+                                processed = true;
                             }
                         }
                     }
@@ -71,14 +74,6 @@ public class ExcelParser {
             }
 
             if (processed) {
-
-                // Create the destination folder if it doesn't exist
-                File destinationFolder = new File(PATH_TO_WRITE).getParentFile();
-                if (!destinationFolder.exists()) {
-                    destinationFolder.mkdirs();
-                }
-
-
                 FileOutputStream fos = new FileOutputStream(PATH_TO_WRITE);
                 destinationWorkbook.write(fos);
                 destinationWorkbook.close();
@@ -86,17 +81,14 @@ public class ExcelParser {
 
                 System.out.println("Parsing complete. Output file saved at: " + PATH_TO_WRITE);
             } else {
-                System.out.println("============= NOTHING DONE =============");
+                System.out.println("Nothing to process.");
             }
-
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     private static String normalizeTelephone(String telephone) {
-
         // Clean the telephone number by removing noise characters
         telephone = telephone.replaceAll("[^0-9]", "");
 
@@ -116,14 +108,24 @@ public class ExcelParser {
     }
 
     private static void loadProperties(Properties prop) {
-        // Load Properties Files !
+        // Load Properties Files
         try (InputStream input = new FileInputStream("src/main/resources/application.properties")) {
-
-            // load a properties file
+            // Load a properties file
             prop.load(input);
-
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+    }
+
+    private static int findExistingPerson(Sheet sheet, String person) {
+        for (int i = 0; i <= sheet.getLastRowNum(); i++) {
+            Row row = sheet.getRow(i);
+            Cell personCell = row.getCell(2); // Column C
+
+            if (personCell != null && personCell.getStringCellValue().equals(person)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
