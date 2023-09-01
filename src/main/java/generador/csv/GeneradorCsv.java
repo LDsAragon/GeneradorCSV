c87f80a9eb6600c261f5542034d7e7a3dd26c659
Matias Salomone <matisalomone@hotmail.com>
2023-09-01 15:40:31 -0300

    Update GeneradorCsv.java

--- src/main/java/generador/csv/GeneradorCsv.java
+++ src/main/java/generador/csv/GeneradorCsv.java
@@ -17,8 +17,6 @@ public class GeneradorCsv {
         final String CSV_NAME = prop.getProperty("csv.name");
         final String EXCEL_NAME = prop.getProperty("excel.name");
         final String DELIMITER = prop.getProperty("csv.delimiter");
-        Boolean processed = false;
-        int rowIndex = 1;
 
         try {
 
@@ -33,29 +31,36 @@ public class GeneradorCsv {
             Sheet sheet = workbook.getSheetAt(0);
 
             // Iterate over rows and columns
-            Iterator<Row> iterator = sheet.iterator();
-            while (iterator.hasNext()) {
-                Row currentRow = iterator.next();
-                Iterator<Cell> cellIterator = currentRow.iterator();
+            for (Row row : sheet) {
+                Iterator<Cell> cellIterator = row.iterator();
+                int currentColumn = 0; // Keep track of the current column index
 
                 while (cellIterator.hasNext()) {
                     Cell currentCell = cellIterator.next();
+                    int cellColumn = currentCell.getAddress().getColumn();
 
-                    // Check if the cell is empty
-                    if (currentCell.getCellType() == CellType.BLANK || currentCell.toString().isEmpty()) {
-                        // If the cell is empty, write an empty string to the CSV
+                    // Fill in empty columns with delimiters
+                    while (currentColumn < cellColumn) {
                         csvWriter.write(DELIMITER);
-                    } else {
-                        // Write cell value to CSV
-                        csvWriter.write(currentCell.toString());
+                        currentColumn++;
                     }
 
-                    // Add a comma between values (customize delimiter as needed)
-                    csvWriter.write(DELIMITER);
+                    // Write cell value to CSV
+                    csvWriter.write(currentCell.toString());
+                    currentColumn++;
+
+                    // Add a comma for all cells except the last one in each row
+                    if (cellIterator.hasNext()) {
+                        csvWriter.write(DELIMITER);
+                    }
                 }
                 // Add a new line character after each row
                 csvWriter.newLine();
             }
+
+            // Close the CSV writer and Excel file
+            csvWriter.close();
+            excelFile.close();
 
             System.out.println("Excel to CSV conversion completed.");
 
